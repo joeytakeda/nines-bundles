4647a1110c75a9962247708bbc884f2259631d46
Michael Joyce <ubermichael@gmail.com>
2019-08-27 13:00:59 -0700

    Make the editor configurable.

--- EditorBundle/DependencyInjection/Configuration.php
+++ EditorBundle/DependencyInjection/Configuration.php
@@ -20,42 +20,6 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('nines_editor');
 
-        $rootNode->children()
-            ->arrayNode('plugins')
-                ->treatNullLike(array())
-                ->prototype('scalar')->end()
-                ->defaultValue(array('image', 'imagetools', 'link', 'lists', 'paste', 'wordcount'))
-                ->end()
-            ->arrayNode('menubar')
-                ->treatNullLike(array())
-                ->prototype('scalar')->end()
-                ->defaultValue(array('edit', 'insert', 'view', 'format', 'tools'))
-                ->end()
-            ->arrayNode('toolbar')
-                ->arrayPrototype()
-                    ->treatNullLike(array())
-                    ->prototype('scalar')->end()
-                    ->defaultValue(array(
-                        ['undo', 'redo'],
-                        ['selectstyle'],
-                        ['paste'],
-                        ['bold', 'italic'],
-                        ['alignleft', 'aligncenter', 'alignright', 'alignjustify'],
-                        ['bulllist', 'numlist', 'outdent', 'indent'],
-                        ['link', 'image'],
-                    ))
-                    ->end()
-                ->end()
-            ->arrayNode('images')
-                ->children()
-                    ->booleanNode('caption')->defaultValue(true)->end()
-                    ->booleanNode('credentials')->defaultValue(true)->end()
-                    ->booleanNode('advanced_tab')->defaultValue(true)->end()
-                    ->booleanNode('title')->defaultValue(true)->end()
-                    ->end()
-                ->end()
-        ;
-
         return $treeBuilder;
     }
 }
